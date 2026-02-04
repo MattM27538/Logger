f1e6e57bc1ef4d5ee7bf1554f8bff8f279ba2ce2
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2026-02-03 20:31:47 -0800

    Add moduleIsEnabled function

--- logger/modulesList.c
+++ logger/modulesList.c
@@ -3,18 +3,33 @@
 #include <assert.h>
 #include <stdbool.h>
 
-ModulesList* modulesListInit(){
+ModulesList* modulesListInit(const int listOfModulesLength){
     static ModulesList* modulesList = NULL;
 
     //Return error or message if modulesList already initialized
     if(modulesList == NULL){
-        modulesList = malloc(sizeof(modulesList));
+        modulesList = malloc(sizeof(modulesList) + (sizeof(char*) * (listOfModulesLength)));
         //Return error if malloc does not work.
         assert(modulesList && "Failed to allocate memory for modulesList");
 
+        modulesList->listOfModulesLength = listOfModulesLength;
         //Leave all values in modulesList null?
     }
 
-
     return modulesList;
 }
+
+bool moduleIsEnabled(const char* moduleName, const ModulesList* listOfModules){
+    assert(listOfModules && "listOfModules is not initialized");
+    assert(moduleName && "moduleName is NULL");
+    assert(moduleName != "" && "moduleName is empty");
+    
+    for(int i = 0; i < listOfModules.listOfModulesLength(); ++i){
+        if(listOfModules[i] == "moduleName"){
+            return true;
+        }
+    }
+    
+    return false;
+}
+
